0a77a3d27cb3ffdd1b5e569f3d31de00fcbe0dd2
sabdou-2 <smariesabdou@gmail.com>
2025-09-13 17:59:27 +0000

    Delete src/main/java/sn/isi/immobilier/dao/UserDao.java

--- src/main/java/sn/isi/immobilier/dao/UserDao.java
+++
@@ -1,9 +0,0 @@
-package sn.isi.immobilier.dao;
-
-import sn.isi.immobilier.model.User;
-
-import java.util.Optional;
-
-public interface UserDao extends CrudDao<User, Long> {
-    Optional<User> findByEmail(String email);
-}
